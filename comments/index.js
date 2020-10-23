3c159aa16264597afbca5a7deb334a52a8eacd78
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-22 21:41:22 -0300

    added the moderation microservice -> testing for moderation service

--- comments/index.js
+++ comments/index.js
@@ -4,7 +4,6 @@ const {randomBytes} = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
 
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,7 +12,6 @@ const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
-  console.log('get:', commentsByPostId[req.params.id]);
 });
 
 app.post('/posts/:id/comments', async (req, res) => {
@@ -22,7 +20,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -31,16 +29,39 @@ app.post('/posts/:id/comments', async (req, res) => {
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   })
 
   res.status(201).send(comments);
-  console.log('post: ', commentsByPostId[req.params.id]);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received event', req.body.type);
+  
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content} = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        postId,
+        id,
+        status,
+        content
+      }
+    });
+  }
+
   res.send({});
 })
 
